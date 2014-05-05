9ec153cd498798ceb21201fe3bf2fc3e9c2cc233
limboy <limboy.me@gmail.com>
2014-05-05 10:48:09 +0800

    Initial Commit

---
+++ HBShare/HBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HBAppDelegate.h
+//  HBShare
+//
+//  Created by Limboy on 5/5/14.
+//  Copyright (c) 2014 Huaban. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HBShare/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HBShare
+//
+//  Created by Limboy on 5/5/14.
+//  Copyright (c) 2014 Huaban. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HBAppDelegate class]));
+    }
+}
